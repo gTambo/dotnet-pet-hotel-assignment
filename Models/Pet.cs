7d9985f97f27f12be7e3e10efc31f4becd356909
Greg Tambo <gregtambornino@gmail.com>
2021-10-28 13:44:50 -0500

    Pet models setup

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,7 +5,46 @@ using System;
 
 namespace pet_hotel
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType 
+    {
+        Shepherd, 
+        Poodle, 
+        Beagle, 
+        Bulldog, 
+        Terrier, 
+        Boxer, 
+        Labrador, 
+        Retriever,
+    }
+    public enum PetColorType 
+    {
+        White, 
+        Black, 
+        Golden, 
+        Tricolor, 
+        Spotted, 
+        Chocolate
+    }
+    public class Pet 
+    {
+        public int id {get; set;}
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetBreedType breed {get; set;}
+
+        [Required]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetColorType color {get; set;}
+        
+        [ForeignKey("PetOwner")]
+        public int petOwnerId {get; set; }
+
+        public PetOwner petOwner {get; set; }
+
+        public DateTime checkedInAt {get; set;}
+    }
 }
